23c8dad5833b3f4d28688d6af51b64e9053d2fc7
adventuresEnglish <149850689+adventuresEnglish@users.noreply.github.com>
2024-05-07 12:21:51 -0500

    fix importing fn into edge env

--- src/lib/auth-no-edge.ts
+++ src/lib/auth-no-edge.ts
@@ -2,13 +2,11 @@ import { authSchema } from "@/lib/validations";
 import bcrypt from "bcryptjs";
 import NextAuth, { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { nextAuthEdgeConfig } from "./auth-edge";
 import { getUserByEmail } from "./server-utils";
 
 const config = {
-  pages: {
-    signIn: "/login",
-    signOut: "/",
-  },
+  ...nextAuthEdgeConfig,
   providers: [
     Credentials({
       async authorize(credentials) {
@@ -40,72 +38,6 @@ const config = {
       },
     }),
   ],
-  callbacks: {
-    authorized: ({ auth, request }) => {
-      //runs on request w middleware
-      const isLoggedIn = Boolean(auth?.user);
-      const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
-
-      if (!isLoggedIn && isTryingToAccessApp) {
-        return false;
-      }
-
-      if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
-        Response.redirect(new URL("/payment", request.nextUrl));
-      }
-      if (isLoggedIn && isTryingToAccessApp && auth?.user.hasAccess) {
-        return true;
-      }
-
-      if (
-        isLoggedIn &&
-        (request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")) &&
-        auth?.user.hasAccess
-      ) {
-        return Response.redirect(new URL("/app/dashboard", request.nextUrl));
-      }
-
-      if (isLoggedIn && !isTryingToAccessApp) {
-        if (
-          (request.nextUrl.pathname.includes("/login") ||
-            request.nextUrl.pathname.includes("/signup")) &&
-          !auth?.user.hasAccess
-        ) {
-          return Response.redirect(new URL("/payment", request.nextUrl));
-        }
-        return true;
-      }
-
-      if (!isLoggedIn && !isTryingToAccessApp) {
-        return true;
-      }
-      return false;
-    },
-    jwt: async ({ token, user, trigger }) => {
-      if (user) {
-        //on sign in
-        token.userId = user.id;
-        token.email = user.email!;
-        token.hasAccess = user.hasAccess;
-      }
-      if (trigger === "update") {
-        // on every request
-        const userFromDb = await getUserByEmail(token.email);
-        if (userFromDb) {
-          token.hasAccess = userFromDb.hasAccess;
-        }
-      }
-
-      return token;
-    },
-    session: ({ session, token }) => {
-      session.user.id = token.userId;
-      session.user.hasAccess = token.hasAccess;
-
-      return session;
-    },
-  },
 } satisfies NextAuthConfig;
 
 export const {
